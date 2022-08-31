57f35c6fbae0ddb8837763742271acc4ce4b7aa2
Niraj <amatyaraj@gmail.com>
2022-08-31 10:57:51 +1000

    Top nav bar styling
    -

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './topbar.scss';
 
 const Topbar = () => {
-  return <div className="topbar">Topbar</div>;
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">Intro</a>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
