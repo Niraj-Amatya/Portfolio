c1c4efacb9f8937403bf875a3f047d0b36379761
Niraj <amatyaraj@gmail.com>
2022-09-02 11:59:27 +1000

    Top navbar left side styling
    - Added logo, further work necessary for styling.
    - Added email address and mobile number.
    - React icons used for icons.
    - Styling added.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './topbar.css';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { HiOutlineMail } from 'react-icons/hi';
 
 const Topbar = () => {
   return (
@@ -7,8 +9,16 @@ const Topbar = () => {
       <div className="header__wrapper">
         <div className="header__left">
           <a href="#intro" className="header__logo">
-            NA
+            N
           </a>
+          <div className="contact-me">
+            <BsFillPersonFill size={25} />
+            <span>0421883784</span>
+          </div>
+          <div className="contact-me">
+            <HiOutlineMail size={25} />
+            <span>amatyaraj@gmail.com</span>
+          </div>
         </div>
         <div className="header__right">hamburger</div>
       </div>
