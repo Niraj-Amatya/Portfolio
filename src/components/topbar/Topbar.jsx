773db9bf3321edb4b14a8d3b98c40f56869e300a
Niraj <amatyaraj@gmail.com>
2022-09-02 22:35:23 +1000

    header style changes depending on the menuIsOpen state

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './topbar.css';
 import { BsFillPersonFill } from 'react-icons/bs';
 import { MdEmail } from 'react-icons/md';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
-const Topbar = () => {
-  const [menuIsOpen, setMenuIsOpen] = useState(false);
-
+const Topbar = ({ handleMenu, menuIsOpen }) => {
+  // menuIsOpen is the state passed as a prop from the App component and can be either true or false depending on the user click events
+  // handleMenu is the function passed from the app. It will change the state of menuIsOpen
+  // helper class to change the style of header, logo and hamburge  when hamburger icon is clicked and state changes.
   const headerClassHelper = menuIsOpen ? 'header active' : 'header';
 
   const logoClassHelper = menuIsOpen
@@ -29,10 +30,7 @@ const Topbar = () => {
             <span>amatyaraj@gmail.com</span>
           </div>
         </div>
-        <div
-          className="header__right"
-          onClick={() => setMenuIsOpen(!menuIsOpen)}
-        >
+        <div className="header__right" onClick={handleMenu}>
           {!menuIsOpen ? (
             <AiOutlineMenu size={30} />
           ) : (
