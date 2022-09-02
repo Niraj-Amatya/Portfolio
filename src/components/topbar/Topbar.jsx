d7b4c26e3e55f34ec3be0b9bf01239a8da724fd2
Niraj <amatyaraj@gmail.com>
2022-09-02 13:46:00 +1000

    Top navbar right handside styling
    - added hamburger menu from react icon.
    - when clicked it show closed icon from react-icon.
    - background color of top navbar and text color changes.

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './topbar.css';
 import { BsFillPersonFill } from 'react-icons/bs';
-import { HiOutlineMail } from 'react-icons/hi';
+import { MdEmail } from 'react-icons/md';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Topbar = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const headerClassHelper = menuIsOpen ? 'header active' : 'header';
+
+  const logoClassHelper = menuIsOpen
+    ? 'header__logo header__logo-change'
+    : 'header__logo';
+
   return (
-    <header className="header">
+    <header className={headerClassHelper}>
       <div className="header__wrapper">
         <div className="header__left">
-          <a href="#intro" className="header__logo">
-            N
+          <a href="#intro" className={logoClassHelper}>
+            N/a
           </a>
           <div className="contact-me">
             <BsFillPersonFill size={25} />
             <span>0421883784</span>
           </div>
           <div className="contact-me">
-            <HiOutlineMail size={25} />
+            <MdEmail size={25} />
             <span>amatyaraj@gmail.com</span>
           </div>
         </div>
-        <div className="header__right">hamburger</div>
+        <div
+          className="header__right"
+          onClick={() => setMenuIsOpen(!menuIsOpen)}
+        >
+          {!menuIsOpen ? (
+            <AiOutlineMenu size={30} />
+          ) : (
+            <AiOutlineClose size={30} />
+          )}
+
+          {/* <div className="header__hamburger">
+            <span className="hamburger__line1"></span>
+            <span className="hamburger__line2"></span>
+            <span className="hamburger__line3"></span>
+          </div> */}
+        </div>
       </div>
     </header>
   );
