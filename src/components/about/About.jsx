5856bdca9bc76d245cfcc27f812b02939f58311b
Niraj <amatyaraj@gmail.com>
2022-09-05 11:38:30 +1000

    styling for the about section header and sub-header

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="about" id="about">
-      Portfolio
-    </div>
+    <section className="about" id="about">
+      <div className="main-container">
+        <h2 className="heading">
+          <span className="heading__sec-main">About Me</span>
+          <span className="heading__sec-sub">
+            Find more information about me and my current skills with
+            programming.
+          </span>
+        </h2>
+      </div>
+    </section>
   );
 };
 
