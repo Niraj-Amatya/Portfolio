0ea79f38f458c7be027f5d2bcbd0b6e78c0d25e3
Niraj <amatyaraj@gmail.com>
2022-09-06 11:53:27 +1000

    added content for about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,6 +12,30 @@ const About = () => {
             programming.
           </span>
         </h2>
+        <div className="about__content">
+          <div className="about__content__main">
+            <h3 className="about__content__title">Know me</h3>
+            <div className="about__content__detail">
+              <p className="about__content__detail-paragraph">
+                I am a <strong>Frontend developer</strong> building frontend
+                solution for better user experience.
+              </p>
+              <p className="about__content__detail-paragraph">
+                I care about the projects and the team I work with. My goal as a{' '}
+                <strong>Web Developer</strong> is to obtain a challenging and
+                rewarding role, where I can further grow and utilise my skills
+                to create simple yet elegant user experience.
+              </p>
+              <p className="about__content__detail-paragraph">
+                I am open to <strong>Job</strong> opportunities where I can
+                contribute, learn and grow to my full potential. If you have a
+                good opportunity that matches to my skills and experience please
+                don't hesitate to <strong>Contact</strong> me.
+              </p>
+            </div>
+          </div>
+          <div className="about__content__skills"></div>
+        </div>
       </div>
     </section>
   );
