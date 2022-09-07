9a857fc7a8d68e002c6ae129ef8e8cb016e707c7
Niraj <amatyaraj@gmail.com>
2022-09-08 09:50:09 +1000

    Component to show the projects
    - Added the content and images.
    - Imported button from the reusable Button Component.
    - Grid and Flexbox used to align differen project content.
    - Media query for different screens.
    - Styling added.

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,11 +1,85 @@
 import React from 'react';
 
 import './project.css';
+import Button from '../UX/button/Button';
+import Img1 from '../../assets/project-1.png';
 
 const Project = () => {
   return (
     <section className="project" id="project">
-      Project
+      <div className="main-container">
+        <h2 className="heading">
+          <span className="heading__sec-main">Projects</span>
+          <span className="heading__sec-sub">
+            This includes all the personal project that I have made during my
+            journey to be a developer.
+          </span>
+        </h2>
+        <div className="project__contents">
+          <div className="project__row">
+            <div className="project__row__img">
+              <img
+                src={Img1}
+                alt="project react one"
+                className="project__img"
+              />
+            </div>
+            <div className="project__row__description">
+              <h3 className="project__title">Order Food Online</h3>
+              <p className="project__desc">
+                An app to order food online using React
+              </p>
+            </div>
+            <Button btnName="Github" />
+          </div>
+
+          {/* ......................... */}
+          <div className="project__row">
+            <div className="project__row__img">
+              <img
+                src={Img1}
+                alt="project react one"
+                className="project__img"
+              />
+            </div>
+            <div className="project__row__description">
+              <h3 className="project__title"></h3>
+              <p className="project__desc"></p>
+            </div>
+            <Button btnName="Github" />
+          </div>
+          {/* ......................... */}
+          <div className="project__row">
+            <div className="project__row__img">
+              <img
+                src={Img1}
+                alt="project react one"
+                className="project__img"
+              />
+            </div>
+            <div className="project__row__description">
+              <h3 className="project__title"></h3>
+              <p className="project__desc"></p>
+            </div>
+            <Button btnName="Github" />
+          </div>
+          {/* ......................... */}
+          <div className="project__row">
+            <div className="project__row__img">
+              <img
+                src={Img1}
+                alt="project react one"
+                className="project__img"
+              />
+            </div>
+            <div className="project__row__description">
+              <h3 className="project__title"></h3>
+              <p className="project__desc"></p>
+            </div>
+            <Button btnName="Github" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
