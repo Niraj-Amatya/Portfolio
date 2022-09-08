e2c871ebaaa7d4bf15b243e08ee2bbb440af48ae
Niraj <amatyaraj@gmail.com>
2022-09-08 12:49:06 +1000

    Create Custom Button
    - Custom button created.
    - This button can be used anywhere in the app and display information depending on the data provided by the component it is used on.
    - The component where it is used will decide what class to use to style the button, what link will it open and how will it look.
    - Recieves data as a props and uses it as its property.

--- src/components/UX/button/Button.jsx
+++ src/components/UX/button/Button.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import './button.css';
 
 const Button = (props) => {
+  console.log(props.btnDetails.btnName);
   return (
     <>
-      <a href="#contact" className="btn">
-        {props.btnName}
+      <a
+        href={props.btnDetails.link}
+        className={props.btnDetails.className}
+        target={props.btnDetails.target ? props.btnDetails.target : ''}
+      >
+        {props.btnDetails.btnName}
       </a>
     </>
   );
