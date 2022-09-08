22cd393c397c3f25fd84219ef6f14bfe84e9898c
Niraj <amatyaraj@gmail.com>
2022-09-08 17:24:33 +1000

    Keep code DRY
    - instead of making seperate div for all the project content, make projects array.
    - object is created inside array for each project, with more project info like title, description, stacks used and images, also link for the buttons.
    - map is used to loop through the projects array and showed in the frontend.
    
    - tech_stacks is an array inside the object, which is also looped and returned to the front.
    - uuid npm package is used to generate individual id for key in the list items.

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -3,8 +3,87 @@ import React from 'react';
 import './project.css';
 import Button from '../UX/button/Button';
 import Img1 from '../../assets/project-1.png';
+import { v4 as uuidv4 } from 'uuid';
 
 const Project = () => {
+  const projects = [
+    {
+      id: 1,
+      title: 'Order Food Online',
+      desc: 'An app to order food online using React',
+      image: Img1,
+      tech_stacks: ['React', 'Javascript', 'Css', 'HTML'],
+      github_link:
+        'https://github.com/Online-Orders/Order-online/blob/master/src/Components/UI/Input.js',
+      website_link:
+        'https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Overflowing_content',
+    },
+    {
+      id: 2,
+
+      title: 'Tours Guide',
+      desc: 'An app to order see places using React',
+      image: Img1,
+      tech_stacks: ['React', 'Javascript', 'Css'],
+      github_link: 'https://github.com/Niraj-Amatya/Counter-App-Redux',
+      website_link: 'https://www.youtube.com/watch?v=8nXqcugV2Y4',
+    },
+    {
+      id: 3,
+      title: 'Food Guide',
+      desc: 'An app to order food online using React',
+      image: Img1,
+      tech_stacks: ['React', 'Javascript', 'Css', 'HTML'],
+      github_link: 'https://github.com/Niraj-Amatya/buymysari',
+      website_link:
+        'https://www.hostinger.com/tutorials/web-developer-portfolio',
+    },
+  ];
+
+  // looping through all the projects from array and returning to the frontend
+  const allProject = projects.map((project) => (
+    <div className="project__row" key={project.id}>
+      <div className="project__row__img">
+        <img
+          src={project.image}
+          alt="project react one"
+          className="project__img"
+        />
+      </div>
+      <div className="project__row__description">
+        <h3 className="project__title">{project.title}</h3>
+        <p className="project__desc">{project.desc}</p>
+        <div className="tech__stacks">
+          {/* getting array of tech stacks and looping thorugh aray to get individual stacks.
+        used uuidv4 npm package to generate unique id for key   */}
+          {project.tech_stacks.map((tech) => (
+            <div className="tech__use" key={uuidv4()}>
+              {tech}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="project__btn">
+        <Button
+          btnDetails={{
+            btnName: 'Github',
+            className: 'btn btn-sm',
+            link: project.github_link,
+            target: '_blank',
+          }}
+        />
+        <Button
+          btnDetails={{
+            btnName: 'Website',
+            className: 'btn btn-primary btn-sm',
+            link: project.website_link,
+            target: '_blank',
+          }}
+        />
+      </div>
+    </div>
+  ));
+
   return (
     <section className="project" id="project">
       <div className="main-container">
@@ -15,87 +94,8 @@ const Project = () => {
             journey to be a developer.
           </span>
         </h2>
-        <div className="project__contents">
-          <div className="project__row">
-            <div className="project__row__img">
-              <img
-                src={Img1}
-                alt="project react one"
-                className="project__img"
-              />
-            </div>
-            <div className="project__row__description">
-              <h3 className="project__title">Order Food Online</h3>
-              <p className="project__desc">
-                An app to order food online using React
-              </p>
-            </div>
-            <div className="project__btn">
-              <Button
-                btnDetails={{
-                  btnName: 'Github',
-                  className: 'btn btn-sm',
-                  link: 'https://github.com/Online-Orders/Order-online/blob/master/src/Components/UI/Input.js',
-                  target: '_blank',
-                }}
-              />
-              <Button
-                btnDetails={{
-                  btnName: 'Website',
-                  className: 'btn btn-primary btn-sm',
-                  link: 'https://github.com/Online-Orders/Order-online/blob/master/src/Components/UI/Input.js',
-                  target: '_blank',
-                }}
-              />
-            </div>
-          </div>
-
-          {/* ......................... */}
-          <div className="project__row">
-            <div className="project__row__img">
-              <img
-                src={Img1}
-                alt="project react one"
-                className="project__img"
-              />
-            </div>
-            <div className="project__row__description">
-              <h3 className="project__title"></h3>
-              <p className="project__desc"></p>
-            </div>
-            {/* <Button btnName="Github" /> */}
-          </div>
-          {/* ......................... */}
-          <div className="project__row">
-            <div className="project__row__img">
-              <img
-                src={Img1}
-                alt="project react one"
-                className="project__img"
-              />
-            </div>
-            <div className="project__row__description">
-              <h3 className="project__title"></h3>
-              <p className="project__desc"></p>
-            </div>
-            {/* <Button btnName="Github" /> */}
-          </div>
-          {/* ......................... */}
-          <div className="project__row">
-            <div className="project__row__img">
-              <img
-                src={Img1}
-                alt="project react one"
-                className="project__img"
-              />
-            </div>
-            <div className="project__row__description">
-              <h3 className="project__title"></h3>
-              <p className="project__desc"></p>
-            </div>
-            {/* <Button btnName="Github" /> */}
-          </div>
-        </div>
+        {/* Content.................... */}
+        <div className="project__contents">{allProject}</div>
       </div>
     </section>
   );
