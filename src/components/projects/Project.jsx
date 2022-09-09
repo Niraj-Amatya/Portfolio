8fda5d907eb8e1b9137bbf097529fd1e8e4cdc22
Niraj <amatyaraj@gmail.com>
2022-09-09 11:24:22 +1000

    Import ProjectView Component.
    - ProjectView component is imported and passed all the project details as props.
    - Styling is added.

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Img1 from '../../assets/project-1.png';
+import ProjectView from './ProjectView';
 
 import './project.css';
-import Button from '../UX/button/Button';
-import Img1 from '../../assets/project-1.png';
-import { v4 as uuidv4 } from 'uuid';
 
 const Project = () => {
-  const projects = [
+  const myProjects = [
     {
       id: 1,
       title: 'Order Food Online',
@@ -38,50 +37,39 @@ const Project = () => {
       website_link:
         'https://www.hostinger.com/tutorials/web-developer-portfolio',
     },
+    {
+      id: 3,
+      title: 'Food Guide',
+      desc: 'An app to order food online using React',
+      image: Img1,
+      tech_stacks: ['React', 'Javascript', 'Css', 'HTML'],
+      github_link: 'https://github.com/Niraj-Amatya/buymysari',
+      website_link:
+        'https://www.hostinger.com/tutorials/web-developer-portfolio',
+    },
+    {
+      id: 3,
+      title: 'Food Guide',
+      desc: 'An app to order food online using React',
+      image: Img1,
+      tech_stacks: ['React', 'Javascript', 'Css', 'HTML'],
+      github_link: 'https://github.com/Niraj-Amatya/buymysari',
+      website_link:
+        'https://www.hostinger.com/tutorials/web-developer-portfolio',
+    },
   ];
 
   // looping through all the projects from array and returning to the frontend
-  const allProject = projects.map((project) => (
-    <div className="project__row" key={project.id}>
-      <div className="project__row__img">
-        <img
-          src={project.image}
-          alt="project react one"
-          className="project__img"
-        />
-      </div>
-      <div className="project__row__description">
-        <h3 className="project__title">{project.title}</h3>
-        <p className="project__desc">{project.desc}</p>
-        <div className="tech__stacks">
-          {/* getting array of tech stacks and looping thorugh aray to get individual stacks.
-        used uuidv4 npm package to generate unique id for key   */}
-          {project.tech_stacks.map((tech) => (
-            <div className="tech__use" key={uuidv4()}>
-              {tech}
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="project__btn">
-        <Button
-          btnDetails={{
-            btnName: 'Github',
-            className: 'btn btn-sm',
-            link: project.github_link,
-            target: '_blank',
-          }}
-        />
-        <Button
-          btnDetails={{
-            btnName: 'Website',
-            className: 'btn btn-primary btn-sm',
-            link: project.website_link,
-            target: '_blank',
-          }}
-        />
-      </div>
-    </div>
+  const projects = myProjects.map((project) => (
+    <ProjectView
+      id={project.id}
+      title={project.title}
+      desc={project.desc}
+      img={project.image}
+      techs={project.tech_stacks}
+      github_link={project.github_link}
+      website_link={project.website_link}
+    />
   ));
 
   return (
@@ -95,7 +83,7 @@ const Project = () => {
           </span>
         </h2>
         {/* Content.................... */}
-        <div className="project__contents">{allProject}</div>
+        <div className="project__contents">{projects}</div>
       </div>
     </section>
   );
