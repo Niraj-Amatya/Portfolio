441845dfc91b4228e973529536c28f90403ec39b
Niraj <amatyaraj@gmail.com>
2022-09-09 17:20:57 +1000

    Contact form to send message.
    - Email.js is used in the contact form to send message.
    - Form fields will be empty once submit.
    - Success message is shown. useState is used.
    - Success message will disappear after settime. useEffect is used.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import emailjs from '@emailjs/browser';
 
 import './contact.css';
 
 const Contact = () => {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const formRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // using email.js to recieve email from contact form
+    emailjs
+      .sendForm(
+        'service_r506el9',
+        'template_3hgcsgm',
+        formRef.current,
+        'SUlKNEffy-rKGR9gs'
+      )
+      .then(
+        (result) => {
+          event.target.reset();
+          setFormSubmitted(true);
+
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  // using useEffect to show hide success message after setinterval of time.
+  useEffect(() => {
+    const clearMessage = setTimeout(() => {
+      setFormSubmitted(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(clearMessage);
+    };
+  }, [formSubmitted]);
+
   return (
     <section className="contact" id="contact">
       <div className="main-container">
@@ -17,7 +55,12 @@ const Contact = () => {
         {/* Contact form */}
 
         <div className="contact__form__container">
-          <form action="#" className="contact__form">
+          <form
+            ref={formRef}
+            action="#"
+            className="contact__form"
+            onSubmit={handleSubmit}
+          >
             <div className="contact__form__field">
               <label htmlFor="name" className="contact__form__label">
                 Name
@@ -72,6 +115,14 @@ const Contact = () => {
               Submit
             </button>
           </form>
+          {formSubmitted ? (
+            <div className="success-message">
+              Your form is been submitted. I will contact you as soon as
+              possible.
+            </div>
+          ) : (
+            ''
+          )}
         </div>
       </div>
     </section>
