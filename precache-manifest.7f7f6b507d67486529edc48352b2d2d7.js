86e21ea810e9c2151b7998e614de2f9f0b57172b
Niraj <amatyaraj@gmail.com>
2022-09-11 19:18:23 +1000

    Updates

---
+++ precache-manifest.7f7f6b507d67486529edc48352b2d2d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "524f55f94366979965f4",
+    "url": "/myportfolio/static/css/main.2017b05c.chunk.css"
+  },
+  {
+    "revision": "524f55f94366979965f4",
+    "url": "/myportfolio/static/js/main.73135185.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  },
+  {
+    "revision": "cee4786dc4ded45e50ad",
+    "url": "/myportfolio/static/js/2.fbd3d95c.chunk.js"
+  },
+  {
+    "revision": "84d8a3ff5157f2910d892eccb2589869",
+    "url": "/myportfolio/static/media/project-1.84d8a3ff.png"
+  },
+  {
+    "revision": "d3ce7ada0e3f14902e43dc5c781ddabd",
+    "url": "/myportfolio/static/media/Niraj-Resume.d3ce7ada.pdf"
+  },
+  {
+    "revision": "a62a444bc629ab698d2acbfde4687670",
+    "url": "/myportfolio/index.html"
+  }
+];
